512d3d5265f6bb989bf76ecbeb0640965abc162c
chungheonLee <fire6376@gmail.com>
2025-04-08 16:32:05 +0900

    Add : Build Setting

--- UnrealEngine/Valorant/Source/Valorant/Valorant.Build.cs
+++ UnrealEngine/Valorant/Source/Valorant/Valorant.Build.cs
@@ -8,6 +8,6 @@ public class Valorant : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
 	}
 }
