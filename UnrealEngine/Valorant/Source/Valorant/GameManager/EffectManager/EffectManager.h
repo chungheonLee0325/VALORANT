e861c692e35bd4bdcca0a0b2da97777425b3242f
chungheonLee <fire6376@gmail.com>
2025-04-09 16:31:17 +0900

    add : EffectManager

---
+++ UnrealEngine/Valorant/Source/Valorant/GameManager/EffectManager/EffectManager.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "EffectManager.generated.h"
+
+class UValorantGameInstance;
+/**
+ * 
+ */
+UCLASS()
+class VALORANT_API UEffectManager : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	void InitManager(UValorantGameInstance* GameInstance);
+
+	UFUNCTION(BlueprintCallable)
+	void ApplyEffectToTarget(AActor* Target, int EffectID, AActor* Instigator = nullptr);
+
+private:
+	UPROPERTY()
+	UValorantGameInstance* m_GameInstance;
+};
