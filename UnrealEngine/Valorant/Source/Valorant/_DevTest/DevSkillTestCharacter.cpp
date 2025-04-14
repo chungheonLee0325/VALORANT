08bf469bffc1b4dd09d6608c3c6552e4f8c56ec1
chungheonLee <fire6376@gmail.com>
2025-04-14 11:01:28 +0900

    add : 개발자 테스트 케릭터 클래스 추가

---
+++ UnrealEngine/Valorant/Source/Valorant/_DevTest/DevSkillTestCharacter.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DevSkillTestCharacter.h"
+
+
+// Sets default values
+ADevSkillTestCharacter::ADevSkillTestCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ADevSkillTestCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADevSkillTestCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ADevSkillTestCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
