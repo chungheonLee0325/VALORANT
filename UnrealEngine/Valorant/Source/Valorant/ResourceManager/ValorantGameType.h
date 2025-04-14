efb7bc7029f7d8aa02d8cfc0c6aafb0c99208c53
chungheonLee <fire6376@gmail.com>
2025-04-14 16:48:06 +0900

    chore : ability 관련 enum, struct 추가 및 수정

--- UnrealEngine/Valorant/Source/Valorant/ResourceManager/ValorantGameType.h
+++ UnrealEngine/Valorant/Source/Valorant/ResourceManager/ValorantGameType.h
@@ -130,6 +130,33 @@ enum class EValorantStatType : uint8
 	UtilityUsage UMETA(DisplayName = "Utility Usage")
 };
 
+// 발로란트 능력 유형 (기본, 시그니처, 궁극기)
+UENUM(BlueprintType)
+enum class EValorantAbilityType : uint8
+{
+	None UMETA(DisplayName = "None"),
+	// 기본 능력 (크레딧으로 구매)
+	Basic UMETA(DisplayName = "Basic"),
+	// 시그니처 능력 (라운드마다 충전)
+	/*
+	*고유 스킬(Signature abilities)은 구매하지 않아도 반드시 한 번은 사용할 수 있으며, 2회 이상 사용하기 위해 특정 조건을 만족하여 충전하거나 별도로 구매할 수 있다. '고유 스킬'인 만큼 그 캐릭터의 전반적인 컨셉이 해당 스킬에 담겨있다.
+레이나, 브림스톤, 스카이, 아스트라, 오멘, 요루, 클로브, 피닉스의 여덟 요원은 2회 이상의 충전량을 구매할 수 있다.
+게코, 데드록, 바이스, 브리치, 사이퍼, 세이지, 소바, 오멘, 체임버, 케이/오, 클로브, 킬조이, 테호, 페이드, 하버의 열다섯 요원은 고유 스킬을 사용한 후 재사용 대기시간을 거칠 때마다 1회씩 충전된다. 이 중 오멘과 클로브는 2회 이상의 충전량을 구매하지 않으면 해당 충전량의 재사용 대기시간이 적용되지 않으며 영구히 충전되지 않는다.
+네온, 레이즈, 요루, 웨이레이, 제트, 피닉스의 여섯 요원은 적 두 명을 처치하여 사용한 스킬을 재충전할 수 있다.
+레이나, 브림스톤, 스카이, 아스트라, 아이소의 다섯 요원은 사용한 스킬이 영구히 충전되지 않는다. 또한 바이퍼는 연료를 소모하는 특수한 매커니즘으로 인해 스킬이 별도로 충전되지 않는다.
+	 */
+	Signature UMETA(DisplayName = "Signature"),
+	// 궁극기 (포인트로 충전)
+	/*
+궁극기(Ultimate abilities)는 궁극기 포인트를 모두 모아야 사용할 수 있다. 궁극기 포인트는 적을 사살, 적에게 사망[1], 스파이크 설치/해체, 매 라운드마다 맵에 배치되는 궁극기 구슬을 습득하여 1개씩 얻을 수 있다. 요원에 따라 6 포인트에서 9 포인트를 획득하면 사용할 수 있다. 궁극기 포인트를 모두 모으면 사용 후 효과가 종료될 때까지 여분의 포인트를 모을 수 없다. 즉, 궁극기를 사용해 적을 처치하면 궁극기 포인트를 얻을 수 없다.
+6 포인트 : 레이나, 피닉스
+7 포인트 : 데드록, 사이퍼, 세이지, 아스트라, 아이소, 오멘, 요루, 하버
+8 포인트 : 레이즈, 바이스, 브림스톤, 스카이, 소바, 웨이레이, 제트, 체임버, 게코, 클로브, 케이/오, 테호, 페이드, 네온
+9 포인트 : 바이퍼, 브리치, 킬조이
+	 */
+	Ultimate UMETA(DisplayName = "Ultimate"),
+};
+
 UENUM(BlueprintType)
 enum class EAbilitySlotType : uint8
 {
@@ -293,6 +320,14 @@ struct FAbilityData : public FTableRowBase
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cooldown")
 	TSubclassOf<UGameplayEffect> CooldownEffectClass = nullptr;
 
+	// 능력 종류
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Valorant|Ability")
+	EValorantAbilityType AbilityType;
+    
+	// 능력 충전 코스트 (시그니처는 라운드당 충전 수, 궁극기는 필요 포인트, 구매 비용)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Valorant|Ability")
+	int32 ChargeCost;
+
 	// 충전 관련
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Charges")
 	bool UsesCharges = false;
@@ -303,12 +338,12 @@ struct FAbilityData : public FTableRowBase
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Charges", meta = (EditCondition = "UsesCharges"))
 	float ChargeRegenTime = 0.0f;
 
-	// 비용 관련
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cost")
-	bool HasUltimateCost = false;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cost", meta = (EditCondition = "HasUltimateCost"))
-	int32 UltimatePointCost = 0;
+	// // 비용 관련
+	// UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cost")
+	// bool HasUltimateCost = false;
+	//
+	// UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Cost", meta = (EditCondition = "HasUltimateCost"))
+	// int32 UltimatePointCost = 0;
 
 	// 효과 관련
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Effects")
