88439e2f588281b34875cf76ddac631be583e3ef
heenchon <heenchon@naver.com>
2025-04-15 21:48:51 +0900

    feat: GameplayTag용 cpp 생성 / Tag 붙은 Ability cpp 생성

---
+++ UnrealEngine/Valorant/Source/Valorant/AbilitySystem/Abilities/GameplayAbilityWithTag.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseGameplayAbility.h"
+#include "GameplayAbilityWithTag.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALORANT_API UGameplayAbilityWithTag : public UBaseGameplayAbility
+{
+	GENERATED_BODY()
+};
---
+++ UnrealEngine/Valorant/Source/Valorant/AbilitySystem/ValorantGameplayTags.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ValorantGameplayTags.h"
+
+
+ValorantGameplayTags::ValorantGameplayTags()
+{
+}
+
+ValorantGameplayTags::~ValorantGameplayTags()
+{
+}
