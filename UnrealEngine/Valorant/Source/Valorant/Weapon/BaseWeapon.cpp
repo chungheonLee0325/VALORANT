07e23de37fa13133b93d7374239ae86e34520539
byungmeo_wanted <wlsrlfvkr@gmail.com>
2025-04-18 11:41:39 +0900

    feat: BaseWeapon 추가 및 런타임 에셋 로드

--- UnrealEngine/Valorant/Source/Valorant/Weapon/BaseWeapon.cpp
+++ UnrealEngine/Valorant/Source/Valorant/Weapon/BaseWeapon.cpp
@@ -3,33 +3,54 @@
 
 #include "BaseWeapon.h"
 
+#include "GameManager/ValorantGameInstance.h"
+#include "Kismet/GameplayStatics.h"
 
-// Sets default values for this component's properties
-UBaseWeapon::UBaseWeapon()
+ABaseWeapon::ABaseWeapon()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh"));
 }
 
-
-// Called when the game starts
-void UBaseWeapon::BeginPlay()
+void ABaseWeapon::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	auto* GameInstance = Cast<UValorantGameInstance>(UGameplayStatics::GetGameInstance(GetWorld()));
+	if (nullptr == GameInstance)
+	{
+		UE_LOG(LogTemp, Error, TEXT("ABaseWeapon::BeginPlay: GameInstance Is Null"));
+		return;
+	}
+
+	WeaponData = GameInstance->GetWeaponData(WeaponID);
+	if (nullptr == WeaponData)
+	{
+		UE_LOG(LogTemp, Error, TEXT("ABaseWeapon::BeginPlay: WeaponData Load Fail (WeaponID : %d)"), WeaponID);
+		return;
+	}
+
+	// TODO: WeaponID에 맞는 SkeletalMesh 불러오기
+	FStringAssetReference MeshRef(TEXT("/Script/Engine.SkeletalMesh'/Game/Resource/FPWeapon/Mesh/SK_FPGun.SK_FPGun'"));
+	auto* WeaponMeshAsset = Cast<USkeletalMesh>(StaticLoadObject(USkeletalMesh::StaticClass(), nullptr, *MeshRef.ToString()));
+	if (nullptr == WeaponMeshAsset || nullptr == WeaponMesh)
+	{
+		UE_LOG(LogTemp, Error, TEXT("ABaseWeapon::BeginPlay: WeaponMeshAsset Load Fail (WeaponID : %d)"), WeaponID);
+		return;
+	}
+	WeaponMesh->SetSkeletalMeshAsset(WeaponMeshAsset);
 	
+	MagazineSize = WeaponData->MagazineSize;
+	MagazineAmmo = MagazineSize;
+	// TODO: 총기별 여분탄약 데이터 추가 필요
+	SpareAmmo = MagazineSize * 3;
+	FireInterval = 1.0f / WeaponData->FireRate;
+	for (auto Element : WeaponData->GunRecoilMap)
+	{
+		RecoilData.Add(Element);
+	}
 }
 
-
-// Called every frame
-void UBaseWeapon::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void ABaseWeapon::Tick(float DeltaSeconds)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
+	Super::Tick(DeltaSeconds);
 }
-
--- UnrealEngine/Valorant/Source/Valorant/Weapon/BaseWeapon.h
+++ UnrealEngine/Valorant/Source/Valorant/Weapon/BaseWeapon.h
@@ -3,25 +3,50 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Components/ActorComponent.h"
+#include "GameFramework/Actor.h"
 #include "BaseWeapon.generated.h"
 
+struct FGunRecoilData;
+struct FWeaponData;
 
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
-class VALORANT_API UBaseWeapon : public UActorComponent
+UCLASS(config=Game)
+class VALORANT_API ABaseWeapon : public AActor
 {
 	GENERATED_BODY()
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Weapon", meta = (AllowPrivateAccess = "true"))
+	int WeaponID = 12;
+
+	FWeaponData* WeaponData = nullptr;
+	TArray<FGunRecoilData> RecoilData;
+	int RecoilLevel = 0;
+	float LastFireTime = -9999.0f;
+	// 발사/사용 주기 (1 / FireRate)
+	float FireInterval = 0.08f;
+	// 탄창 크기
+	int MagazineSize = 0;
+	// 탄창 내 남은 탄약
+	int MagazineAmmo = 0;
+	// 여분 탄약 (장전되어있는 탄창 내 탄약은 제외)
+	int SpareAmmo = 0;
+	
+	UPROPERTY()
+	TObjectPtr<USkeletalMeshComponent> WeaponMesh;
+
+	// UPROPERTY()
+	// TObjectPtr<UWidgetComponent> PickUpUIComponent;
+
+	// UPROPERTY()
+	// TObjectPtr<USphereComponent> PickUpComponent;
+
 public:
 	// Sets default values for this component's properties
-	UBaseWeapon();
+	ABaseWeapon();
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
 public:
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
-	                           FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void Tick(float DeltaSeconds) override;
 };
