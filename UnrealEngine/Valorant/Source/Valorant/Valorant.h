efcf388adae7a92cd0703dc4b303c17bce384fa1
byungmeo_wanted <wlsrlfvkr@gmail.com>
2025-04-21 10:10:31 +0900

    feat: 로그 관련 매크로 선언

--- UnrealEngine/Valorant/Source/Valorant/Valorant.h
+++ UnrealEngine/Valorant/Source/Valorant/Valorant.h
@@ -3,3 +3,38 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+/**
+ * NetMode에 따라 접두사가 달라지고 이외는 모두 동일한 로그
+ */
+#define NET_LOG(CategoryName, Verbosity, Format, ...) \
+	{ \
+		FString Prefix; \
+		if (auto* World = GetWorld()) \
+		{ \
+			ENetMode NetMode = World->GetNetMode(); \
+			if (NetMode == NM_DedicatedServer || NetMode == NM_ListenServer) \
+			{ \
+				Prefix = TEXT("[SERVER] "); \
+			} \
+			else if (NetMode == NM_Client) \
+			{ \
+				Prefix = TEXT("[CLIENT] "); \
+			} \
+			else \
+			{ \
+				Prefix = TEXT("[STANDALONE] "); \
+			} \
+		} \
+		else \
+		{ \
+			Prefix = TEXT("[UNKNOWN] "); \
+		} \
+		UE_LOG(CategoryName, Verbosity, TEXT("%s%s"), *Prefix, *FString::Printf(Format, ##__VA_ARGS__)); \
+	}
+
+#define SCREEN_LOG(Format, ...) \
+	if (GEngine) \
+	{ \
+		GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Red, *FString::Printf(Format, ##__VA_ARGS__)); \
+	}
