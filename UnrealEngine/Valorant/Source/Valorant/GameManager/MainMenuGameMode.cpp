3e78cd400d1d429613a49910a4c003cad9b91edf
byungmeo_wanted <wlsrlfvkr@gmail.com>
2025-04-23 13:36:02 +0900

    feat: MainMap 전용 GameMode 생성

---
+++ UnrealEngine/Valorant/Source/Valorant/GameManager/MainMenuGameMode.cpp
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainMenuGameMode.h"
+
+#include "Valorant.h"
+
+AMainMenuGameMode::AMainMenuGameMode()
+{
+}
+
+void AMainMenuGameMode::PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId,
+	FString& ErrorMessage)
+{
+	Super::PreLogin(Options, Address, UniqueId, ErrorMessage);
+	NET_LOG(LogTemp, Warning, TEXT("AMainMenuGameMode::PreLogin Options: %s, Address: %s, UniqueId: %s, ErrorMessage: %s"), *Options, *Address, *(UniqueId.IsValid() ? UniqueId->ToString() : FString(TEXT("INVALID"))), *ErrorMessage);
+}
+
+void AMainMenuGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+	const auto Address = NewPlayer->GetPlayerNetworkAddress();
+	const auto UniqueId = NewPlayer->GetUniqueID();
+	NET_LOG(LogTemp, Warning, TEXT("AMainMenuGameMode::PostLogin Address: %s, UniqueId: %d"), *Address, UniqueId);
+}
---
+++ UnrealEngine/Valorant/Source/Valorant/GameManager/MainMenuGameMode.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "MainMenuGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALORANT_API AMainMenuGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AMainMenuGameMode();
+
+protected:
+	virtual void PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage) override;
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+};
