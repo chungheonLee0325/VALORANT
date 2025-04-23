e3946cf4ca025bdadfa3ee5f01882850c6b6b0f5
heenchon <heenchon@naver.com>
2025-04-23 15:13:55 +0900

    feat: Base어빌리티, 스킬 Input System 연동

--- UnrealEngine/Valorant/Source/Valorant/AbilitySystem/Abilities/BaseGameplayAbility.h
+++ UnrealEngine/Valorant/Source/Valorant/AbilitySystem/Abilities/BaseGameplayAbility.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Abilities/GameplayAbility.h"
+#include "AbilitySystem/ValorantGameplayTags.h"
 #include "BaseGameplayAbility.generated.h"
 
 /**
@@ -14,12 +15,30 @@ class VALORANT_API UBaseGameplayAbility : public UGameplayAbility
 {
 	GENERATED_BODY()
 
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Input")
+	TSet<FGameplayTag> FollowUpInputTags;
+
+	UPROPERTY()
+	FGameplayTag CurrentFollowUpInputTag;
+
 	//TODO: CanActivateAbility(): 스킬 실행 가능한지 체크, 쿨타임 블락태그 등
+	virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags = nullptr, const FGameplayTagContainer* TargetTags = nullptr, FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
+	
 	//TODO: ActiveAbility(): 스킬 가짜 실행, (필요하다면) ASC에 후속 입력 키 등록
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	
 	//TODO: InputPressd(): 스킬 진짜 실행
+	virtual void InputPressed(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
+	
 	//TODO: InputReleased()
-	//TODO: EndAbility(): ASC->ClearCurrnetAbility 호출
-	//TODO: CancelAbility(): ASC->ClearCurrnetAbility 호출
+	virtual void InputReleased(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
+	
+	//TODO: EndAbility(): ASC->ClearCurrentAbilityHandle 호출
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	
+	//TODO: CancelAbility(): ASC->ClearCurrentAbilityHandle 호출
+	virtual void CancelAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateCancelAbility) override;
 
 	// Ability에서 ASC가져오는 법?
 	// UAbilitySystemComponent* MyASC = GetAbilitySystemComponentFromActorInfo();
