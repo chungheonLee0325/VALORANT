d5398c4972c8ff1e9daa71515672b53da99eaf2e
byungmeo_wanted <wlsrlfvkr@gmail.com>
2025-04-25 10:41:59 +0900

    feat: HttpManager

---
+++ UnrealEngine/Valorant/Source/Valorant/Web/HttpTestUI.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HttpTestUI.h"
+
+#include "HttpManager.h"
+
+void UHttpTestUI::OnHttpResponseReceived(int32 ResponseCode, const FString& ResponseBody)
+{
+	
+}
+
+void UHttpTestUI::SendRequest()
+{
+	auto* HttpManager = UHttpManager::GetInstance();
+	HttpManager->OnHttpResponseReceived.AddDynamic(this, &UHttpTestUI::OnHttpResponseReceived);
+
+	FString Url = "127.0.0.1:8000/report/generation";
+	FHttpQuery Query;
+	Query.AddQuery("damage", "3500");
+	Query.AddQuery("test", "wow");
+	Url = Query.AppendToURL(Url);
+	HttpManager->SendRequest(Url);
+	
+/*
+	LogHttpManager: UHttpManager::SendRequest Called, HTTP Request Info:
+		URL: 127.0.0.1:8000/report/generation?damage=3500&test=wow
+		Verb: GET
+		Header: Content-Type: application/json
+		Body: 
+ */
+}
---
+++ UnrealEngine/Valorant/Source/Valorant/Web/HttpTestUI.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HttpTestUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALORANT_API UHttpTestUI : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION()
+	void OnHttpResponseReceived(int32 ResponseCode, const FString& ResponseBody);
+	UFUNCTION(BlueprintCallable)
+	void SendRequest();
+};
