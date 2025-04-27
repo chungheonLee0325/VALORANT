04ca9f7fb59f86f500caab6826721558f074cea8
choiyeontaek <dusxorchl@naver.com>
2025-04-27 18:26:09 +0900

    chore: BaseAgent (미니맵아이콘, 해당캐릭터의상태, 시야각체크) 진행중

---
+++ UnrealEngine/Valorant/Source/Valorant/Player/Widget/MiniMapWidget.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MiniMapWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALORANT_API UMiniMapWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
