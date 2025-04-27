76ad460e9f98795b9885317b4cc540d46eaeacfe
byungmeo_wanted <wlsrlfvkr@gmail.com>
2025-04-27 18:20:44 +0900

    feat: 게임 시작 시 캐릭터 선택 UI 표시

--- UnrealEngine/Valorant/Source/Valorant/GameManager/MatchGameMode.h
+++ UnrealEngine/Valorant/Source/Valorant/GameManager/MatchGameMode.h
@@ -6,7 +6,20 @@
 #include "GameFramework/GameMode.h"
 #include "MatchGameMode.generated.h"
 
+class AMatchPlayerController;
+class USubsystemSteamManager;
 class UValorantGameInstance;
+
+UENUM(BlueprintType)
+enum class EMatchPhase : uint8
+{
+	Phase_Select,
+	Phase_Buy,
+	Phase_Round,
+	Phase_Intermission,
+	Phase_EndMatch
+};
+
 /**
  * 
  */
@@ -15,10 +28,6 @@ class VALORANT_API AMatchGameMode : public AGameMode
 {
 	GENERATED_BODY()
 
-protected:
-	UPROPERTY()
-	TObjectPtr<UValorantGameInstance> ValorantGameInstance = nullptr;
-	
 public:
 	AMatchGameMode();
 
@@ -26,4 +35,23 @@ protected:
 	virtual void BeginPlay() override;
 	virtual void PreLogin(const FString& Options, const FString& Address, const FUniqueNetIdRepl& UniqueId, FString& ErrorMessage) override;
 	virtual void PostLogin(APlayerController* NewPlayer) override;
+
+private:
+	UPROPERTY()
+	TObjectPtr<UValorantGameInstance> ValorantGameInstance = nullptr;
+	UPROPERTY()
+	TObjectPtr<USubsystemSteamManager> SubsystemManager = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category="Multiplayer", meta=(AllowPrivateAccess))
+	int RequiredPlayerCount = 9999;
+	UPROPERTY(BlueprintReadOnly, Category="Multiplayer", meta=(AllowPrivateAccess))
+	int LoggedInPlayerNum = 0;
+	UPROPERTY(BlueprintReadOnly, Category="Gameflow", meta=(AllowPrivateAccess))
+	EMatchPhase CurrentMatchPhase = EMatchPhase::Phase_Select;
+	UPROPERTY(BlueprintReadOnly, Category="Gameflow", meta=(AllowPrivateAccess))
+	TSet<AMatchPlayerController*> PlayerControllerSet;
+	
+public:
+	void OnControllerBeginPlay(AMatchPlayerController* Controller);
+	void StartSelectPhase();
 };
---
+++ UnrealEngine/Valorant/Source/Valorant/Player/MatchPlayerController.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GameManager/MatchGameMode.h"
+#include "MatchPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALORANT_API AMatchPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	AMatchPlayerController();
+	
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	// GameMode의 PostLogin 단계에서 주입된다
+	UPROPERTY()
+	TObjectPtr<AMatchGameMode> GameMode = nullptr;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> SelectUIWidgetClass;
+	UPROPERTY()
+	TObjectPtr<UUserWidget> SelectUIWidget = nullptr;
+
+public:
+	void SetGameMode(AMatchGameMode* MatchGameMode);
+	UFUNCTION(Server, Reliable)
+	void ServerRPC_NotifyBeginPlay();
+	UFUNCTION(Client, Reliable)
+	void ClientRPC_DisplaySelectUI();
+};
