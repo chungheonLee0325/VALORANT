daa7306a4b4f708c586e8d66652aa41b86e12397
chungheonLee <fire6376@gmail.com>
2025-04-28 16:01:22 +0900

    chore : 병합용

--- UnrealEngine/Valorant/Source/Valorant/GameManager/MatchGameMode.cpp
+++ UnrealEngine/Valorant/Source/Valorant/GameManager/MatchGameMode.cpp
@@ -8,6 +8,8 @@
 #include "Valorant.h"
 #include "GameManager/ValorantGameInstance.h"
 #include "Player/MatchPlayerController.h"
+#include "Player/AgentPlayerState.h"
+#include "Player/Component/CreditComponent.h"
 
 AMatchGameMode::AMatchGameMode()
 {
@@ -80,8 +82,208 @@ void AMatchGameMode::OnControllerBeginPlay(AMatchPlayerController* Controller)
 
 void AMatchGameMode::StartSelectPhase()
 {
+	CurrentMatchPhase = EMatchPhase::Phase_Select;
+	
 	for (auto* Controller : PlayerControllerSet)
 	{
 		Controller->ClientRPC_DisplaySelectUI();
+	}
+}
+
+void AMatchGameMode::StartBuyPhase()
+{
+	CurrentMatchPhase = EMatchPhase::Phase_Buy;
+	
+	// 모든 플레이어에게 크레딧 리셋 (첫 라운드) 또는 보상
+	if (CurrentRound == 0)
+	{
+		// 첫 라운드 시작: 크레딧 초기화
+		for (auto* Controller : PlayerControllerSet)
+		{
+			AAgentPlayerState* PlayerState = Controller->GetPlayerState<AAgentPlayerState>();
+			if (PlayerState && PlayerState->GetCreditComponent())
+			{
+				PlayerState->GetCreditComponent()->ResetCredits();
+			}
+		}
+	}
+	
+	// 바이 페이즈 UI 표시
+	for (auto* Controller : PlayerControllerSet)
+	{
+		Controller->ClientRPC_DisplayBuyUI();
+	}
+}
+
+void AMatchGameMode::StartRound()
+{
+	CurrentMatchPhase = EMatchPhase::Phase_Round;
+	CurrentRound++;
+	
+	// 바이 페이즈 UI 닫기
+	for (auto* Controller : PlayerControllerSet)
+	{
+		Controller->ClientRPC_HideBuyUI();
+	}
+}
+
+void AMatchGameMode::FinishRound(EValorantTeam WinTeam)
+{
+	CurrentMatchPhase = EMatchPhase::Phase_Intermission;
+	this->WinningTeam = WinTeam;
+	
+	// 승리 팀에 점수 추가
+	if (WinTeam == EValorantTeam::Team_Attacker)
+	{
+		AttackerScore++;
+	}
+	else if (WinTeam == EValorantTeam::Team_Defender)
+	{
+		DefenderScore++;
+	}
+	
+	// 크레딧 보상 지급
+	for (auto* Controller : PlayerControllerSet)
+	{
+		AAgentPlayerState* PlayerState = Controller->GetPlayerState<AAgentPlayerState>();
+		if (PlayerState && PlayerState->GetCreditComponent())
+		{
+			EValorantTeam PlayerTeam = Controller->GetTeam();
+			
+			if (PlayerTeam == WinTeam)
+			{
+				PlayerState->GetCreditComponent()->RewardRoundWin();
+			}
+			else
+			{
+				PlayerState->GetCreditComponent()->RewardRoundLose();
+			}
+		}
+	}
+	
+	// 스파이크 상태 리셋
+	bIsSpikeActive = false;
+	bIsSpikeDefused = false;
+	
+	// 게임 종료 체크
+	bool bGameEnded = (AttackerScore > MaxRounds / 2) || (DefenderScore > MaxRounds / 2);
+	
+	if (bGameEnded || CurrentRound >= MaxRounds)
+	{
+		// 게임 종료
+		CurrentMatchPhase = EMatchPhase::Phase_EndMatch;
+		
+		// 게임 종료 처리
+		for (auto* Controller : PlayerControllerSet)
+		{
+			Controller->ClientRPC_DisplayEndMatchUI();
+		}
+	}
+	else
+	{
+		// 다음 라운드 준비 - 바이 페이즈로 전환
+		FTimerHandle TimerHandle;
+		GetWorldTimerManager().SetTimer(TimerHandle, this, &AMatchGameMode::StartBuyPhase, 5.0f, false);
+	}
+}
+
+void AMatchGameMode::OnSpikeActive()
+{
+	bIsSpikeActive = true;
+	
+	// 스파이크 설치한 플레이어에게 크레딧 보상
+	for (auto* Controller : PlayerControllerSet)
+	{
+		if (Controller->GetTeam() == EValorantTeam::Team_Attacker && Controller->DidPlantSpike())
+		{
+			AAgentPlayerState* PlayerState = Controller->GetPlayerState<AAgentPlayerState>();
+			if (PlayerState && PlayerState->GetCreditComponent())
+			{
+				PlayerState->GetCreditComponent()->RewardPlantSpike();
+			}
+		}
+	}
+}
+
+void AMatchGameMode::OnSpikeDefused()
+{
+	bIsSpikeDefused = true;
+	bIsSpikeActive = false;
+	
+	// 스파이크 해제한 플레이어에게 크레딧 보상
+	for (auto* Controller : PlayerControllerSet)
+	{
+		if (Controller->GetTeam() == EValorantTeam::Team_Defender && Controller->DidDefuseSpike())
+		{
+			AAgentPlayerState* PlayerState = Controller->GetPlayerState<AAgentPlayerState>();
+			if (PlayerState && PlayerState->GetCreditComponent())
+			{
+				PlayerState->GetCreditComponent()->RewardDefuseSpike();
+			}
+		}
+	}
+	
+	// 수비팀 승리로 라운드 종료
+	FinishRound(EValorantTeam::Team_Defender);
+}
+
+void AMatchGameMode::OnPlayerKilled(AMatchPlayerController* Victim, AMatchPlayerController* Killer)
+{
+	// 킬러가 존재하고, 자기 자신을 죽인 것이 아니라면 킬 크레딧 지급
+	if (Killer && Killer != Victim)
+	{
+		AAgentPlayerState* PlayerState = Killer->GetPlayerState<AAgentPlayerState>();
+		if (PlayerState && PlayerState->GetCreditComponent())
+		{
+			PlayerState->GetCreditComponent()->RewardKill();
+		}
+	}
+	
+	// 모든 플레이어의 생존 상태 확인
+	bool bAllAttackersDead = true;
+	bool bAllDefendersDead = true;
+	
+	for (auto* Controller : PlayerControllerSet)
+	{
+		if (!Controller->IsDead())
+		{
+			if (Controller->GetTeam() == EValorantTeam::Team_Attacker)
+			{
+				bAllAttackersDead = false;
+			}
+			else if (Controller->GetTeam() == EValorantTeam::Team_Defender)
+			{
+				bAllDefendersDead = false;
+			}
+		}
+	}
+	
+	// 라운드 승패 결정
+	if (bIsSpikeActive)
+	{
+		// 스파이크가 설치된 상태
+		if (bAllDefendersDead)
+		{
+			// 수비팀 전멸 - 공격팀 승리
+			FinishRound(EValorantTeam::Team_Attacker);
+		}
+		else if (bAllAttackersDead)
+		{
+			// 공격팀 전멸 - 결정되지 않음 (스파이크가 터질 때까지 대기)
+		}
+	}
+	else
+	{
+		// 스파이크가 설치되지 않은 상태
+		if (bAllAttackersDead)
+		{
+			// 공격팀 전멸 - 수비팀 승리
+			FinishRound(EValorantTeam::Team_Defender);
+		}
+		else if (bAllDefendersDead)
+		{
+			// 수비팀 전멸 - 공격팀 승리
+			FinishRound(EValorantTeam::Team_Attacker);
+		}
 	}
 }
--- UnrealEngine/Valorant/Source/Valorant/GameManager/MatchGameMode.h
+++ UnrealEngine/Valorant/Source/Valorant/GameManager/MatchGameMode.h
@@ -20,6 +20,15 @@ enum class EMatchPhase : uint8
 	Phase_EndMatch
 };
 
+// 발로란트 팀 Enum
+UENUM(BlueprintType)
+enum class EValorantTeam : uint8
+{
+	Team_None,
+	Team_Attacker,
+	Team_Defender
+};
+
 /**
  * 
  */
@@ -51,7 +60,52 @@ private:
 	UPROPERTY(BlueprintReadOnly, Category="Gameflow", meta=(AllowPrivateAccess))
 	TSet<AMatchPlayerController*> PlayerControllerSet;
 	
+#pragma region Gameflow
+	// 게임 진행 관련 변수
+	UPROPERTY(BlueprintReadOnly, Category="Gameflow", meta=(AllowPrivateAccess))
+	int32 CurrentRound = 0;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Gameflow", meta=(AllowPrivateAccess))
+	int32 MaxRounds = 24;
+	
+	UPROPERTY(BlueprintReadOnly, Category="Gameflow", meta=(AllowPrivateAccess))
+	int32 AttackerScore = 0;
+	
+	UPROPERTY(BlueprintReadOnly, Category="Gameflow", meta=(AllowPrivateAccess))
+	int32 DefenderScore = 0;
+	
+	UPROPERTY(BlueprintReadOnly, Category="Gameflow", meta=(AllowPrivateAccess))
+	EValorantTeam WinningTeam = EValorantTeam::Team_None;
+	
+	// 스파이크 관련 변수
+	UPROPERTY(BlueprintReadOnly, Category="Gameflow", meta=(AllowPrivateAccess))
+	bool bIsSpikeActive = false;
+	
+	UPROPERTY(BlueprintReadOnly, Category="Gameflow", meta=(AllowPrivateAccess))
+	bool bIsSpikeDefused = false;
+
+#pragma endregion
+	
 public:
 	void OnControllerBeginPlay(AMatchPlayerController* Controller);
 	void StartSelectPhase();
+	
+	// 게임 진행 관련 함수
+	UFUNCTION(BlueprintCallable)
+	void StartBuyPhase();
+	
+	UFUNCTION(BlueprintCallable)
+	void StartRound();
+	
+	UFUNCTION(BlueprintCallable)
+	void FinishRound(EValorantTeam WinTeam);
+	
+	UFUNCTION(BlueprintCallable)
+	void OnSpikeActive();
+	
+	UFUNCTION(BlueprintCallable)
+	void OnSpikeDefused();
+	
+	UFUNCTION(BlueprintCallable)
+	void OnPlayerKilled(AMatchPlayerController* Victim, AMatchPlayerController* Killer);
 };
