997ef3fca071d07c5e9ad6e7d4fc2e25bc429338
byungmeo_wanted <wlsrlfvkr@gmail.com>
2025-05-14 19:16:45 +0900

    feat: 스킬 투사체 테스트를 위한 세팅

---
+++ UnrealEngine/Valorant/Source/Valorant/Projectile/Fireball.cpp
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Fireball.h"
+
+#include "FireGround.h"
+
+
+// Sets default values
+AFireball::AFireball()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AFireball::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFireball::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AFireball::OnProjectileBounced(const FHitResult& ImpactResult, const FVector& ImpactVelocity)
+{
+	Super::OnProjectileBounced(ImpactResult, ImpactVelocity);
+	if (ImpactResult.ImpactNormal.Z > 0.5f)
+	{
+		FActorSpawnParameters SpawnParameters;
+		SpawnParameters.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+		SpawnParameters.Instigator = this->GetInstigator();
+		GetWorld()->SpawnActor<AFireGround>(FireGroundClass, ImpactResult.ImpactPoint, FRotator::ZeroRotator, SpawnParameters);
+		Destroy();
+	}
+}
---
+++ UnrealEngine/Valorant/Source/Valorant/Projectile/Fireball.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseProjectile.h"
+#include "Fireball.generated.h"
+
+class AFireGround;
+
+UCLASS()
+class VALORANT_API AFireball : public ABaseProjectile
+{
+	GENERATED_BODY()
+
+public:
+	AFireball();
+	
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AFireGround> FireGroundClass = nullptr;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	virtual void OnProjectileBounced(const FHitResult& ImpactResult, const FVector& ImpactVelocity) override;
+};
