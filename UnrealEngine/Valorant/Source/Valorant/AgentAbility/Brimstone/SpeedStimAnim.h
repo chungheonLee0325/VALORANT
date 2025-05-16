601a4910ef05a4c20cc09be337676525e55ecd50
byungmeo_wanted <wlsrlfvkr@gmail.com>
2025-05-16 17:33:34 +0900

    feat: 브림스톤 스피드스팀 에셋 임포트 및 ABP 생성

---
+++ UnrealEngine/Valorant/Source/Valorant/AgentAbility/Brimstone/SpeedStimAnim.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SpeedStimAnim.generated.h"
+
+UENUM(BlueprintType)
+enum class ESpeedStimState : uint8
+{
+	ESSS_Idle,
+	ESSS_Active,
+	ESSS_Outtro
+};
+
+/**
+ * 
+ */
+UCLASS()
+class VALORANT_API USpeedStimAnim : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TObjectPtr<USkeletalMeshComponent> Mesh = nullptr;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	ESpeedStimState State = ESpeedStimState::ESSS_Idle;
+	
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnDeploy();
+};
