a8ed2267bd5bda831107bd3537680400cb6ae290
chungheonLee <fire6376@gmail.com>
2025-05-20 18:39:45 +0900

    chore : 제트 스킬 e, q, c 구현

--- UnrealEngine/Valorant/Source/Valorant/AbilitySystem/Abilities/Jett/Jett_C_Cloudburst.h
+++ UnrealEngine/Valorant/Source/Valorant/AbilitySystem/Abilities/Jett/Jett_C_Cloudburst.h
@@ -9,4 +9,9 @@ class VALORANT_API UJett_C_Cloudburst : public UBaseGameplayAbility
     GENERATED_BODY()
 
     UJett_C_Cloudburst();
+
+    virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+    UPROPERTY(EditDefaultsOnly)
+    UAnimMontage* AbilityMontage = nullptr;
 }; 
--- UnrealEngine/Valorant/Source/Valorant/AbilitySystem/Abilities/Jett/Jett_Q_Updraft.h
+++ UnrealEngine/Valorant/Source/Valorant/AbilitySystem/Abilities/Jett/Jett_Q_Updraft.h
@@ -9,4 +9,6 @@ class VALORANT_API UJett_Q_Updraft : public UBaseGameplayAbility
     GENERATED_BODY()
 
     UJett_Q_Updraft();
+    
+    virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 }; 
