c328fec69953fa9960fd0ce51fe03db742911ec7
chungheonLee <fire6376@gmail.com>
2025-05-20 21:07:03 +0900

    chore : 도트힐 구현, 방벽 구현중

--- UnrealEngine/Valorant/Source/Valorant/AbilitySystem/Abilities/Sage/Sage_E_HealingOrb.h
+++ UnrealEngine/Valorant/Source/Valorant/AbilitySystem/Abilities/Sage/Sage_E_HealingOrb.h
@@ -1,12 +1,30 @@
 #pragma once
 
 #include "AbilitySystem/Abilities/BaseGameplayAbility.h"
+#include "_DevTest/GA_LeftRight.h"
 #include "Sage_E_HealingOrb.generated.h"
 
 UCLASS()
-class VALORANT_API USage_E_HealingOrb : public UBaseGameplayAbility
+class VALORANT_API USage_E_HealingOrb : public UGA_LeftRight
 {
     GENERATED_BODY()
 
     USage_E_HealingOrb();
+
+protected:
+    // 힐링에 사용할 변수들
+    UPROPERTY(EditDefaultsOnly, Category="Heal")
+    float HealAmount = 100.f;
+    UPROPERTY(EditDefaultsOnly, Category="Heal")
+    float HealDuration = 2.f;
+    UPROPERTY(EditDefaultsOnly, Category="Heal")
+    float HealTickInterval = 0.2f;
+    UPROPERTY(EditDefaultsOnly, Category="Heal")
+    float HealTraceDistance = 1000.f;
+
+    UPROPERTY(EditAnywhere)
+    TSubclassOf<UGameplayEffect> GameplayEffect = nullptr;
+
+    virtual void Active_Left_Click(FGameplayEventData data) override;
+    virtual void Active_Right_Click(FGameplayEventData data) override;
 }; 
