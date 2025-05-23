834d71372632b61222327bdaf34044a91053c729
chungheonLee <fire6376@gmail.com>
2025-05-23 09:38:34 +0900

    chore : 세이지 c 배리어 구현(회전기능 가구현)

--- UnrealEngine/Valorant/Source/Valorant/AbilitySystem/Abilities/Sage/Sage_C_BarrierOrb.cpp
+++ UnrealEngine/Valorant/Source/Valorant/AbilitySystem/Abilities/Sage/Sage_C_BarrierOrb.cpp
@@ -9,15 +9,19 @@ USage_C_BarrierOrb::USage_C_BarrierOrb()
 	Tags.AddTag(FGameplayTag::RequestGameplayTag(FName("Input.Skill.C")));
 	SetAssetTags(Tags);
 
-	//FollowUpInputTags.Add(FGameplayTag::RequestGameplayTag(FName("Input.Default.LeftClick")));
-	//FollowUpInputTags.Add(FGameplayTag::RequestGameplayTag(FName("Input.Default.RightClick")));
+	// === 후속 입력 설정 (CDO에서 안전한 방식) ===
+	ValidFollowUpInputs.Add(FGameplayTag::RequestGameplayTag(FName("Input.Default.LeftClick")));
+	ValidFollowUpInputs.Add(FGameplayTag::RequestGameplayTag(FName("Input.Default.RightClick")));
 	
 	m_AbilityID = 1001;
+	InputType = EAbilityInputType::MultiPhase;
 	BarrierRotation = FRotator::ZeroRotator;
 }
 
-void USage_C_BarrierOrb::Active_Left_Click(FGameplayEventData data)
+void USage_C_BarrierOrb::HandleLeftClick(FGameplayEventData EventData)
 {
+	Super::HandleLeftClick(EventData);
+
 	// 장벽 생성 위치 계산 (플레이어 앞쪽)
 	AActor* Owner = CachedActorInfo.AvatarActor.Get();
 	if (!Owner || !BarrierClass) return;
@@ -26,10 +30,37 @@ void USage_C_BarrierOrb::Active_Left_Click(FGameplayEventData data)
 	FRotator SpawnRot = BarrierRotation;
 	// 실제 장벽 스폰
 	GetWorld()->SpawnActor<AActor>(BarrierClass, SpawnLoc, SpawnRot);
+	TransitionToState(FValorantGameplayTags::Get().State_Ability_Executing);
+	CommitAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo);
 }
 
-void USage_C_BarrierOrb::Active_Right_Click(FGameplayEventData data)
+void USage_C_BarrierOrb::HandleRightClick(FGameplayEventData EventData)
 {
+	Super::HandleRightClick(EventData);
+
 	// 장벽 회전값 변경
 	BarrierRotation.Yaw += BarrierYawStep;
+	
+	// 디버그 박스 미리보기
+	AActor* Owner = CachedActorInfo.AvatarActor.Get();
+	if (!Owner) return;
+
+	FVector SpawnLoc = Owner->GetActorLocation() + Owner->GetActorForwardVector() * BarrierSpawnDistance;
+	SpawnLoc.Z += BarrierSpawnHeight;
+	FRotator SpawnRot = BarrierRotation;
+
+	FVector BoxExtent(150.f, 50.f, 100.f); // UE는 반크기
+	FQuat BoxQuat = SpawnRot.Quaternion();
+
+	DrawDebugBox(
+		GetWorld(),
+		SpawnLoc,
+		BoxExtent,
+		BoxQuat,
+		FColor::Green,
+		false,      // bPersistentLines
+		0.7f,       // LifeTime
+		0,          // DepthPriority
+		2.0f        // Thickness
+	);
 }
